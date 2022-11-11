efada98003214f73d692cff148f7505ff20e6da2
yayo21aj <111710385+yayo21aj@users.noreply.github.com>
2022-11-10 21:54:46 -0600

    Create Nodo.java

---
+++ TablaHash/Nodo.java
@@ -0,0 +1,23 @@
+package com.mycompany.tablahash;
+
+public class Nodo {
+
+   private int valor;
+   private Nodo siguiente;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
